31b478f130a9c0fe84224810159d20cba66bed28
Karol Jurzec <karol.jurzec.programista@gmail.com>
2026-07-15 15:57:15 +0200

    Move project files to repository root

---
+++ src/App/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+include __DIR__ ."/../../vendor/autoload.php";
+
+use Framework\App;
+
+use function App\Config\registerRoutes;
+
+$app = new App();
+
+registerRoutes($app);
+
+return $app;
