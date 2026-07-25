e3be5b9d11de12685298c922f34bd459a1fcafde
Karol Jurzec <karol.jurzec.programista@gmail.com>
2026-07-25 17:03:56 +0200

    Create containers and use dependency injection

--- src/App/Controllers/AboutController.php
+++ src/App/Controllers/AboutController.php
@@ -9,15 +9,10 @@ use App\Config\Paths;
 
 class AboutController
 {
-  private TemplateEngine $view;
-
-  public function __construct()
-  {
-    $this->view = new TemplateEngine(Paths::VIEW);
+  public function __construct(private TemplateEngine $view) {
   }
 
-  public function about()
-  {
+  public function about() {
     echo $this->view->render('about.php', [
       'title' => 'About',
       'dangerousData' => '<script>alert(123)</script>'
