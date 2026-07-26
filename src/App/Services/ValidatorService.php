e40636777de4dd7253b25773d4e2fe61330f350f
Karol Jurzec <karol.jurzec.programista@gmail.com>
2026-07-26 03:10:29 +0200

    Apllying rules to form fields

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -4,16 +4,28 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Framework\Validator;
+use Framework\Rules\{RequiredRule};
 
 class ValidatorService {
   private Validator $validator;
 
   public function __construct() {
     $this->validator = new Validator();  //set validator property to a new instance of validator class
+
+    $this->validator->add('required', new RequiredRule());
   }
 
   public function validateRegister(array $formData) {
-    $this->validator->validate($formData);
+    $this->validator->validate($formData, [
+      //for each input form field add the required rule aliases
+      'email' => ['required'],
+      'age' => ['required'],
+      'country' => ['required'],
+      'socialMediaURL' => ['required'],
+      'password' => ['required'],
+      'confirmPassword' => ['required'],
+      'terms' => ['required']
+    ]);
   }
 }
 ?>
--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -3,8 +3,26 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use Framework\Contracts\RuleInterface;
+
 class Validator {
-  public function validate(array $formData) {
-    dd($formData);
+  private array $rules = [];
+
+  public function add(string $alias, RuleInterface $rule) {
+    $this->rules[$alias] = $rule;
+  }
+
+  public function validate(array $formData, array $fields) {
+    foreach($fields as $fieldName => $rules) {     //single form input with rules array
+      foreach($rules as $rule) {                  //single rule from array rules
+        $ruleValidator = $this->rules[$rule];     //assign rule to variable
+
+        if($ruleValidator->validate($formData, $fieldName, [])) {
+          continue;
+        }
+
+        echo "Error";
+      }
+    }
   }
 }
